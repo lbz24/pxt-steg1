ea1bed2f685d67d32fa3367e5525318ff1253f24
lbz24 <161616366+lbz24@users.noreply.github.com>
2024-02-28 15:12:25 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
-function leds () {
-	
-}
 input.onButtonPressed(Button.A, function () {
-    cryptsteg.showImage(Images.Crown)
+	
 })
 input.onButtonPressed(Button.B, function () {
-    cryptsteg.showNextImage()
+	
 })
-cryptsteg.clearDisplay()
