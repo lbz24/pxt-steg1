70ff6f8dbb7578bf6009498a2e8866a10e9971fa
lbz24 <161616366+lbz24@users.noreply.github.com>
2024-03-05 23:28:56 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    cryptsteg.showNextStegImage()
-})
-input.onButtonPressed(Button.AB, function () {
-    cryptsteg.resetAllImages()
+	
 })
 input.onButtonPressed(Button.B, function () {
-    cryptsteg.clearDisplay()
+	
 })
-cryptsteg.showStegImg(Images.Heart)
